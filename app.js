948d3a1f9c1b40d07e262609d41c5d1e11dbd539
最爱滕小厨 <teng520jiao@qq.com>
2019-01-12 00:26:19 +0800

    支付

--- app.js
+++ app.js
@@ -9,22 +9,23 @@ App({
 
   getUserInfo() {
     return new Promise((resolve, reject) => {
-      if (this.userInfo) resolve(this.userInfo);
+      //if (this.userInfo) resolve(this.userInfo);
 
       my.getAuthCode({
         scopes: ['auth_user'],
         success: authcode => {
-          console.info(authcode);
+          resolve(authcode)
+          //console.info(authcode);
 
-          my.getAuthUserInfo({
-            success: res => {
-              this.userInfo = res;
-              resolve(this.userInfo);
-            },
-            fail: () => {
-              reject({});
-            },
-          });
+          // my.getAuthUserInfo({
+          //   success: res => {
+          //     this.userInfo = res;
+          //     resolve(this.userInfo);
+          //   },
+          //   fail: () => {
+          //     reject({});
+          //   },
+          // });
         },
         fail: () => {
           reject({});
--- pages/todos/todos.js
+++ pages/todos/todos.js
@@ -3,16 +3,98 @@ const app = getApp();
 import sha512 from 'js-sha512'
 
 Page({
-	data: {},
+	data: {
+    userid: '',
+    totalamount: 0
+  },
 
 	onLoad() {
-		console.log(new Date().getTime())
-    app.getUserInfo().then(res => {
-      console.log(res)
+		//console.log(new Date().getTime())
+    app.getUserInfo().then(resp => {
+      console.log(resp)
+      my.httpRequest({
+        url: 'https://alipay.futuresmile.org.cn/getAliUserId', // 目标服务器url
+        data: {
+          auth_code:resp.authCode
+        },
+        success: (res) => {
+          console.log(res)
+          if(res.data.message == 'success'){
+            this.setData({
+              userid: res.data.user_id
+            })
+          }
+        },
+        fail: (err) => {
+          console.log(err)
+        }
+      });
     }).catch(err => {})
 	},
   btnTap(){
-    
+    my.showLoading({
+      content: '加载中'
+    });
+    let tradeno = new Date().getTime()
+    let totalamount = 0.01
+    let subject = '为兔唇宝宝的爱心捐赠'
+    let buyerid = this.data.userid
+    let sign = sha512(tradeno+'|'+totalamount+'|'+subject+'|'+buyerid)
+    my.httpRequest({
+      url: 'https://alipay.futuresmile.org.cn/getAliTradeNo', // 目标服务器url
+      method: 'POST',
+      data: {
+        out_trade_no: tradeno,
+        total_amount: totalamount,
+        subject: subject,
+        buyer_id: buyerid,
+        sign: sign
+      },
+      success: (res) => {
+        console.log(res)
+        if(res.data.code == '0000' && res.data.message == 'success'){
+          my.tradePay({
+            tradeNO: res.data.trade_no,
+            success: (resp) => {
+              my.alert({
+                title: '温馨提示',
+                content: '感谢您的爱心',
+                success: () =>{
+                  my.hideLoading();
+                }
+              });
+            },
+            fail: err => {
+              my.alert({
+                title: '温馨提示',
+                content: '支付失败',
+                success: () =>{
+                  my.hideLoading();
+                }
+              });
+            }
+          });
+        }else{
+          my.alert({
+            title: '温馨提示',
+            content: '支付失败',
+            success: () =>{
+              my.hideLoading();
+            }
+          });
+        }
+      },
+      fail: err => {
+        console.log(err)
+        my.alert({
+          title: '温馨提示',
+          content: '支付失败',
+          success: () =>{
+            my.hideLoading();
+          }
+        });
+      }
+    });
   },
   error(e){
     console.log(e)
